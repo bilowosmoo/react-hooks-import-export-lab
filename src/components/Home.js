edd7f5b842dd14764aaac3cb384354758748feac
bilow osmoo <bilowosmo@gmail.com>
2025-04-14 22:16:10 +0300

    complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,9 @@ function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {"username"} is a Web Developer from {"city"}
       </h1>
     </div>
   );
 }
+export default Home;
